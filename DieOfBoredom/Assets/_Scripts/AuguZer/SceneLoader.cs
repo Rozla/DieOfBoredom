e18d227bf90b01b6474d044b2312755158c67a92
AuguZer <augumahieu@hotmail.fr>
2023-07-30 18:37:59 +0200

    r

--- DieOfBoredom/Assets/_Scripts/AuguZer/SceneLoader.cs
+++ DieOfBoredom/Assets/_Scripts/AuguZer/SceneLoader.cs
@@ -2,23 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] InputActionAsset inputActions;
+
+    private void Update()
+    {
+        if (inputActions.FindAction("Pause").WasPressedThisFrame())
+        {
+            Debug.Log("izi");
+        }
+    }
 
     public void LoadScene(int builtIndex)
     {
         SceneManager.LoadScene(builtIndex);
     }
 
-    public void ExitGame()
-    {
-#if UNITY_STANDALONE
-        Application.Quit();
-#endif
+//    public void ExitGame()
+//    {
+//#if UNITY_STANDALONE
+//        Application.Quit();
+//#endif
 
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
-    }
+//#if UNITY_EDITOR
+//        UnityEditor.EditorApplication.isPlaying = false;
+//#endif
+//    }
 }
