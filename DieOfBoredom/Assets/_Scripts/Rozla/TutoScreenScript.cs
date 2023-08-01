edc675cd281f4755975ebd76b4dac7edbcff57c4
Rozla <vincent.laroze.vl@gmail.com>
2023-08-01 21:24:49 +0200

    Add tuto scene before level 1

---
+++ DieOfBoredom/Assets/_Scripts/Rozla/TutoScreenScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class TutoScreenScript : MonoBehaviour
+{
+    [SerializeField] GameObject _continueButton;
+    [SerializeField] EventSystem _eventSystem;
+
+    PlayerInput _playerInput;
+
+    string _gamePad = "Gamepad";
+    string _knm = "Keyboard&Mouse";
+
+    bool _canCheck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _playerInput.onControlsChanged += OnControlsChanged;
+
+        StartCoroutine(ScreenCor());
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+
+    void OnControlsChanged(PlayerInput obj)
+    {
+        
+
+        if (!_canCheck) return;
+
+        Debug.Log(_playerInput.currentControlScheme);
+
+        if (_playerInput.currentControlScheme == _gamePad)
+        {
+            Cursor.visible = false;
+            _eventSystem.SetSelectedGameObject(_continueButton.gameObject);
+        }
+
+        if (_playerInput.currentControlScheme == _knm)
+        {
+            Cursor.visible = true;
+            _eventSystem.SetSelectedGameObject(null);
+        }
+    }
+
+    IEnumerator ScreenCor()
+    {
+        yield return new WaitForSeconds(3f);
+
+        _continueButton.SetActive(true);
+        _canCheck = true;
+        _eventSystem.SetSelectedGameObject(_continueButton.gameObject);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+    }
+}
