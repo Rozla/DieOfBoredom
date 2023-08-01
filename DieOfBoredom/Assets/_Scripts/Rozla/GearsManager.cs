ac8886de4d4b5882c0527e8ab6c16368b1a5eadb
Rozla <vincent.laroze.vl@gmail.com>
2023-08-02 01:03:27 +0200

    fix gear manager

--- DieOfBoredom/Assets/_Scripts/Rozla/GearsManager.cs
+++ DieOfBoredom/Assets/_Scripts/Rozla/GearsManager.cs
@@ -7,20 +7,4 @@ using UnityEngine.Rendering;
 public class GearsManager : MonoBehaviour
 {
     public static List<GearInList> _allGears = new List<GearInList>();
-
-    private void OnDrawGizmos()
-    {
-        Handles.zTest = CompareFunction.LessEqual;
-
-        foreach(GearInList gear in _allGears)
-        {
-            Vector3 managerPos = transform.position;
-            Vector3 playerPos = gear.transform.position;
-            float halfHeight = (managerPos.y - playerPos.y) * .5f;
-            Vector3 offset = Vector3.up * halfHeight;
-
-
-            Handles.DrawBezier(managerPos, playerPos, managerPos - offset, playerPos + offset, Color.white, EditorGUIUtility.whiteTexture, 2f);
-        }
-    }
 }
